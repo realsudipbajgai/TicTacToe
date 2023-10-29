8e77b1192897f27ded92953812c38790664e4a53
Sudip <83912230+realsudipbajgai@users.noreply.github.com>
2023-10-29 12:33:22 -0500

    Create cross on click complete

--- script.js
+++ script.js
@@ -6,11 +6,41 @@ window.onload=function () {
 }
 function createCircleCross(e) {
     createCircle(e);
+    // createCross(e);
 }
 function createCircle(e) {
     const newDiv = document.createElement("div");
     newDiv.classList.add('circle');
     e.target.appendChild(newDiv);
-
 }
 
+function createCross(e)
+{
+    var cross_one=createCrossOne();
+    var cross_two = createCrossTwo();
+ 
+    /*
+    Structure of cross div
+    <div clsas="cross">
+        <div class="cross-one">\</div>
+        <div class="cross-two">/</div>
+    </div>
+    */
+    const mainCross = document.createElement("div");
+    mainCross.classList.add('cross');
+    mainCross.appendChild(cross_one);
+    mainCross.appendChild(cross_two);
+    e.target.appendChild(mainCross);
+}
+
+function createCrossOne() {
+    const newDiv = document.createElement("div");
+    newDiv.classList.add('cross-one');
+    return newDiv;
+}
+
+function createCrossTwo() {
+    const newDiv = document.createElement("div");
+    newDiv.classList.add('cross-two');
+    return newDiv;
+}
